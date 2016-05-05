2d6a95a8d53c3a5ea1e09254af900789aeb20fea
Baumelbi <Baumelbi@gmail.com>
2016-05-04 21:01:29 -0700

    Added files via upload

---
+++ sudoku/src/csc143/sudoku/SudokuBase.java
@@ -0,0 +1,104 @@
+package csc143.sudoku;
+
+/**
+ *
+ */
+public abstract class SudokuBase extends java.util.Observable {
+
+    private final int rows;
+    private final int columns;
+    private final int size;
+
+    public enum State {COMPLETE, INCOMPLETE, ERROR}
+
+    /**
+     * This is the constructor for the Sudoku base
+     * @param layoutRows
+     * @param layoutColumns
+     */
+    public SudokuBase(int layoutRows, int layoutColumns) {
+        rows = layoutRows;
+        columns = layoutColumns;
+        size = columns * rows;
+    }
+
+    /**
+     * This method gives you the number of rows
+     * @return rows the amount of rows
+     */
+    public int getRows() {
+        return rows;
+    }
+
+    /**
+     * This method gives you the number of columns
+     * @return columns the amount of columns
+     */
+    public int getColumns() {
+        return columns;
+    }
+
+    /**
+     * This method gives you the board
+     * @return size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * This abstract class provides you with the value at a given location
+     * on the board
+     * @param row
+     * @param col
+     * @return value the value at the location
+     */
+    public abstract int getValue(int row, int col);
+
+    /**
+     * This abstract method allows you to set the value at a given location
+     * @param row
+     * @param col
+     * @param value
+     */
+    public abstract void setValue(int row, int col, int value);
+
+    /**
+     * This abstrast method tells you if the selected spot is a "given"
+     * location. meaning that it is a fixed value.
+     * @param row
+     * @param col
+     * @return
+     */
+    public abstract boolean isGiven(int row, int col);
+
+    /**
+     *This method lets you set a position as a "given" location
+     */
+    public abstract void fixGivens();
+
+    /**
+     * This method takes in the row number and lets you know if the
+     * row is incomplete, complete, or error
+     * @param n the row number
+     * @return
+     */
+    public abstract State getRowState(int n);
+
+    /**
+     * This method takes in the column and lets you know if the
+     * row is incomplete, complete, or error
+     * @param n the column number
+     * @return
+     */
+    public abstract State getColumnState(int n);
+
+    /**
+     * This method takes in the region number and lets you know
+     * if the region is complete, incomplete or error
+     * @param n the region number
+     * @return
+     */
+    public abstract State getRegionState(int n);
+
+}
---
+++ sudoku/src/csc143/sudoku/SudokuBoard.java
@@ -0,0 +1,142 @@
+package csc143.sudoku;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SudokuBoard extends javax.swing.JComponent implements SelectedCell {
+
+    private int size;
+    private SudokuBase b;
+
+    //Use the layout manager for this project.
+
+    /**
+     * This is the constrcutor sudoku board and will provide the instrutions for creating a proper board
+     * based on the given amount of rows and columns wanted for the desired game
+     *
+     * @param b sudoku base
+     */
+    public SudokuBoard(SudokuBase b) {
+        //setting variables from SudokuBase methods
+        int x = 1;
+        int y = 1;
+        //rows
+        int rows = b.getRows();
+        //columns
+        int columns = b.getColumns();
+        //size = rows*columns
+        size = b.getSize();
+        Square[][] board = new Square[size][size];
+        setPreferredSize(new Dimension(size * 51 + b.getRows() + 1, size * 51 + b.getColumns() + 1));
+
+        for (int i = 0; i < columns; i++) {
+
+            for (int j = 0; j < rows; j++) {
+                y += 1;
+
+                for (int k = 0; k < rows; k++) {
+
+                    for (int l = 0; l < columns; l++) {
+                        x += 1;
+                        if (i % 2 == 0 && k % 2 != 0 || i % 2 != 0 && k % 2 == 0) {
+                            Color gainsboro = new Color(220, 220, 220);
+                            Square square = new Square(x, y, gainsboro);
+                            add(square);
+                            board[(x + 1) / 50][(y + 1) / 50] = square;
+                        } else {
+                            Color white = Color.white;
+                            Square square = new Square(x, y, white);
+                            add(square);
+                            board[(x + 1) / 50][(y + 1) / 50] = square;
+                        }
+                        x += 50;
+                    }
+                }
+                x = 1;
+                y += 50;
+            }
+        }
+
+    }
+
+    /**
+     * paintComponent with Graphics g as a parameter and is used to great the border of the SudokuBoard for the square units.
+     *
+     * @param g - Graphics.
+     */
+    protected void paintComponent(Graphics g) {
+        //overriding
+        super.paintComponent(g);
+        //setting color as white base
+        g.setColor(Color.WHITE);
+        //filling the base
+        g.fillRect(0, 0, (size * 51 + 2), (size * 51 + 2));
+        //border
+        g.setColor(Color.BLACK);
+        g.drawRect(0, 0, (size * 51 + 2), (size * 51 + 2));
+    }
+
+    class Square extends JPanel {
+        //instance fields
+        //variables
+        int width = 50;
+        int height = 50;
+        Color ogColor;
+
+        /**
+         * Constructor square is where each square is made.  Holds the constructor, x and y, for location of each square, and parameter c for color that
+         * will be set when used.
+         *
+         * @param x - x location
+         * @param y - y location
+         * @param c - color set when used
+         */
+        private Square(int x, int y, Color c) {
+            //set color when used
+            this.ogColor = c;
+            //50x50 dimension
+            setSize(width, height);
+            //location
+            setLocation(x, y);
+            //setting a black border for visibility
+            setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            //set color once used
+            setBackground(c);
+        }
+    }
+
+    SudokuBase getBase() {
+        return b;
+    }
+
+    /**
+     * Set the selected cell to the given row and column.
+     *
+     * @param row The indicated row
+     * @param col The indicated column
+     */
+    @Override
+    public void setSelected(int row, int col) {
+
+    }
+
+    /**
+     * Retrive the row of the currently selected cell.
+     *
+     * @return The row in which the selected cell is located.
+     */
+    @Override
+    public int getSelectedRow() {
+        return 0;
+    }
+
+    /**
+     * Retrive the column of the currently selected cell.
+     *
+     * @return The column in which the selected cell is located.
+     */
+    @Override
+    public int getSelectedColumn() {
+        return 0;
+    }
+}
